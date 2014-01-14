85ae7bb9bf540b6b37fe2f9f155aa494e50652fe
David Bordoley <bordoley@gmail.com>
2014-01-13 20:07:07 -0800

    Change virtualHostApplicationSupplier() to take function, and require a default application. Remove MimeContentInfo and use ContentInfo.wrapHeaders(). replace MultipartInputProcessor class with parseMultipartInput function.

--- lib/io.dart
+++ lib/io.dart
@@ -6,8 +6,6 @@ import "package:mime/mime.dart";
 
 import "package:restlib_common/collections.dart";
 import "package:restlib_common/objects.dart";
-import "package:restlib_common/preconditions.dart";
-import "package:restlib_parsing/parsing.dart";
 
 import "package:restlib_core/data.dart";
 import "package:restlib_core/http.dart";
@@ -21,16 +19,10 @@ part "src/io/io_resource.dart";
 
 typedef Application ApplicationSupplier(Request);
 
-ApplicationSupplier virtualHostApplicationSupplier(Map<String,Application> applications, [Application fallback]) {
-  checkArgument(applications.isNotEmpty);
-
-  fallback = (fallback != null) ? fallback : applications.values.first;
-
-  return (Request request){
-    Application app = applications[request.uri.host];
-    return (app != null) ? app : fallback;
-  };
-}
+ApplicationSupplier virtualHostApplicationSupplier(Option<Application> applicationForHost(String host), final Application fallback) =>
+    (final Request request) =>
+        applicationForHost(request.uri.host)
+          .orElse(fallback);
 
 Future<Request<String>> parseString(final Request request, final Stream<List<int>> msgStream){
   final Charset charset = 
@@ -83,113 +75,26 @@ Future writeMultipart(final Request request, final Response<MultipartOutput> res
 }
 
 
-typedef Option<PartParser> PartParserProvider(ContentInfo contentInfo);
 typedef Future<Part> PartParser(ContentInfo contentInfo, final Stream<List<int>> msgStream);
 
-class MultipartInputProcessor {
-  final PartParserProvider partParserProvider;
+Future<Request<Multipart>> parseMultipartInput(final Request request, final Stream<List<int>> msgStream, Option<PartParser> partParserProvider(ContentInfo contentInfo)){
+  final String boundary = request.contentInfo.mediaRange.value.parameters["boundary"].value;
   
-  MultipartInputProcessor(this.partParserProvider);
-
-  Future<Request<Multipart>> parseMultipartInput(final Request request, final Stream<List<int>> msgStream){
-    final String boundary = request.contentInfo.mediaRange.value.parameters["boundary"].value;
-    
-    return new MimeMultipartTransformer(boundary)
-      .bind(msgStream)
-      .map((final MimeMultipart multipart) => 
-              new Part(new _MimeContentInfo(multipart), multipart))
-      .fold(Persistent.EMPTY_SEQUENCE, (final ImmutableSequence<Future> futureResults, final Part<Stream<List<int>>> part) => 
-          partParserProvider(part.contentInfo)
-            .map((final PartParser parser) =>
-                futureResults.add(
-                    parser(part.contentInfo, part.entity)))
-            .orCompute(() =>
-                futureResults.add(part.entity.drain()))
-      .then(Future.wait)
-      .then((final List parts) => 
-          parts.where((final e) =>
-              e is Part))
-      .then((final List<Part> parts) =>
-          request.with_(entity: new Multipart(parts))));              
-   }
+  return new MimeMultipartTransformer(boundary)
+    .bind(msgStream)
+    .map((final MimeMultipart multipart) => 
+        new Part(new ContentInfo.wrapHeaders(new Dictionary.wrapMap(multipart.headers)), multipart))
+    .fold(Persistent.EMPTY_SEQUENCE, (final ImmutableSequence<Future> futureResults, final Part<Stream<List<int>>> part) => 
+        partParserProvider(part.contentInfo)
+          .map((final PartParser parser) =>
+              futureResults.add(parser(part.contentInfo, part.entity)))
+          .orCompute(() =>
+              futureResults.add(part.entity.drain()))
+     .then(Future.wait)
+     .then((final List parts) => 
+         parts.where((final e) =>
+             e is Part))
+     .then((final List<Part> parts) =>
+         request.with_(entity: new Multipart(parts))));              
 }
 
-// Copied pretty much verbatim from http_request_wrapper
-class _MimeContentInfo 
-    extends Object
-    with ContentInfoWith_
-    implements PartContentInfo {
-  final MimeMultipart delegate;
-  
-  ImmutableSequence<ContentEncoding> _encodings;
-  ImmutableSet<Language> _languages;
-  Option<int> _length;
-  Option<Uri> _location;
-  Option<MediaRange> _mediaRange;
-  Option<ContentRange> _range;
-  
-  _MimeContentInfo(this.delegate);
-  
-  Option _parse(final Parser parser, final Header header) =>
-      // FIXME: If the same headers is used more than once this doesn't work
-      parser.parse(nullToEmpty(delegate.headers[header.toString()]));
-  
-  ImmutableSequence<ContentEncoding> get encodings =>
-      computeIfNull(_encodings, () {
-        _encodings = 
-            _parse(CONTENT_ENCODING_HEADER, Header.CONTENT_ENCODING)
-              .map((final Iterable<ContentEncoding> encodings) => 
-                  Persistent.EMPTY_SEQUENCE.addAll(encodings))
-              .orElse(Persistent.EMPTY_SEQUENCE);
-            
-        return _encodings;
-      });
-  
-  ImmutableSet<Language> get languages =>
-      computeIfNull(_languages, () {
-        _languages = 
-            _parse(CONTENT_LANGUAGE, Header.CONTENT_LANGUAGE)
-              .map((final Iterable<Language> languages) => 
-                  Persistent.EMPTY_SET.addAll(languages))
-              .orElse(Persistent.EMPTY_SET);    
-            
-        return _languages;
-      });
- 
-  Option<int> get length =>
-      computeIfNull(_length, () {
-        _length = _parse(INTEGER, Header.CONTENT_LENGTH);
-        return _length;
-      });
-  
-  Option<Uri> get location =>
-      computeIfNull(_location, () {
-        _location = _parseUri(nullToEmpty(delegate.headers[Header.CONTENT_LOCATION.toString()]));
-        return _location;
-      });
-  
-  Option<MediaRange> get mediaRange =>
-      computeIfNull(_mediaRange, () {
-        _mediaRange = _parse(MEDIA_RANGE, Header.CONTENT_TYPE);
-        return _mediaRange;
-      });
-  
-  Option<ContentRange> get range =>
-      computeIfNull(_range, () {
-        _range = _parse(CONTENT_RANGE, Header.CONTENT_RANGE);
-        return _range;
-      });
-}
-
-// Copied pretty much verbatim from http_request_wrapper
-Option<Uri> _parseUri(final String uri) {
-  checkNotNull(uri);
-
-  if (uri.isEmpty) {
-    return Option.NONE;
-  } else {
-    // FIXME: Try catch for exceptions.
-    final Uri result = Uri.parse(uri);
-    return new Option(result);
-  }
-}
