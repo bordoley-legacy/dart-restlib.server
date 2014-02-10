5c7f7defdf3d71f055f9f70970496a75a8d40e4a
David Bordoley <bordoley@gmail.com>
2014-02-09 22:52:17 -0800

    Move future responses into core lib.

--- lib/server.dart
+++ lib/server.dart
@@ -18,13 +18,13 @@ import "package:restlib_parsing/parsing.dart";
 import "package:restlib_core/data.dart";
 import "package:restlib_core/data.media_ranges.dart";
 import "package:restlib_core/http.dart";
+import "package:restlib_core/http.future_responses.dart";
 import "package:restlib_core/multipart.dart";
 import "package:restlib_core/net.dart";
 
 part "src/server/authorizer.dart";
 part "src/server/authorizing_resource.dart";
 part "src/server/byte_range_resource.dart";
-part "src/server/future_responses.dart";
 part "src/server/resource.dart";
 part "src/server/route.dart";
 part "src/server/uniform_resource.dart";
