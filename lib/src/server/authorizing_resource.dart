6cb3aa6bd58edac79083f77e7c801b9df9557407
David Bordoley <bordoley@gmail.com>
2014-02-27 22:11:54 -0800

    remove proxy authenticated resource. It doesn't make sense practically.

--- lib/src/server/authorizing_resource.dart
+++ lib/src/server/authorizing_resource.dart
@@ -26,30 +26,3 @@ class _AuthorizingResource<T> extends Object with ForwardingResource<T> {
                       authenticated ? super.handle(request) : CLIENT_ERROR_FORBIDDEN ))
         ).orElse(_unauthorizedResponse);
 }
-
-class _ProxyAuthorizingResource<T> extends Object with ForwardingResource<T> {
-  final ImmutableDictionary<String, Authorizer> _authorizerMap;
-  final Future<Response> _unauthorizedResponse;
-  final Resource<T> delegate;
-
-  _ProxyAuthorizingResource(this.delegate, final ImmutableDictionary<String, Authorizer> authorizerMap) :
-    this._authorizerMap = authorizerMap,
-    this._unauthorizedResponse =
-      new Future.value(
-        new Response(
-            statuses.CLIENT_ERROR_PROXY_AUTHENTICATED,
-            entity : statuses.CLIENT_ERROR_PROXY_AUTHENTICATED.reason,
-            authenticationChallenges : authorizerMap.map((final Pair<String, Authorizer> pair) =>
-                pair.snd.authenticationChallenge)));
-
-  Future<Response> handle(final Request request) =>
-      request.proxyAuthorizationCredentials
-        .flatMap((final ChallengeMessage message) =>
-          _authorizerMap[message.scheme.toLowerCase()]
-            .map((final Authorizer authorizer) =>
-                authorizer
-                  .authenticate(request)
-                  .then((final bool authenticated) =>
-                      authenticated ? super.handle(request) : CLIENT_ERROR_FORBIDDEN ))
-        ).orElse(_unauthorizedResponse);
-}
--- lib/src/server/resource.dart
+++ lib/src/server/resource.dart
@@ -1,30 +1,22 @@
 part of restlib.server;
 
-abstract class Resource<T> {  
+abstract class Resource<T> {
   factory Resource.authorizingResource(final Resource<T> delegate, final Iterable<Authorizer> authorizer) {
-    final ImmutableDictionary<String, Authorizer> authorizerMap = 
+    final ImmutableDictionary<String, Authorizer> authorizerMap =
         EMPTY_DICTIONARY.putAll(authorizer.map((final Authorizer authorizer) =>
             new Pair(authorizer.scheme, authorizer)));
-    
+
     return new _AuthorizingResource<T> (delegate, authorizerMap);
   }
-  
+
   factory Resource.byteRangeResource(final Resource<T> delegate) =>
       new _ByteRangeResource(delegate);
-  
+
   factory Resource.uniform(final UniformResourceDelegate<T> delegate) {
     final ImmutableSet<Method> allowedMethods = UniformResourceDelegate._getImplementedMethods(delegate);
     return new _UniformResource._internal(delegate, allowedMethods);
   }
-  
-  factory Resource.proxyAuthorizingResource(final Resource<T> delegate, final Iterable<Authorizer> authorizer) {
-    final ImmutableDictionary<String, Authorizer> authorizerMap = 
-        EMPTY_DICTIONARY.putAll(authorizer.map((final Authorizer authorizer) =>
-            new Pair(authorizer.scheme, authorizer)));
-    
-    return new _ProxyAuthorizingResource<T> (delegate, authorizerMap);
-  }
-  
+
   Route get route;
 
   Request filterRequest(final Request request);
@@ -33,16 +25,16 @@ abstract class Resource<T> {
   Future<Response> acceptMessage(Request<T> request);
 }
 
-abstract class ForwardingResource<T> implements Forwarder, Resource<T> {    
+abstract class ForwardingResource<T> implements Forwarder, Resource<T> {
   Route get route => delegate.route;
 
   Request filterRequest(final Request request) =>
       delegate.filterRequest(request);
-  
+
   Response filterResponse(final Response response) =>
       delegate.filterResponse(response);
-  
+
   Future<Response> handle(final Request request) => delegate.handle(request);
-  
+
   Future<Response> acceptMessage(final Request<T> request) => delegate.acceptMessage(request);
 }
